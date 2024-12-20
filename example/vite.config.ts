6cc9e9e3f394adf5410e8bb403b29eb1cf793ab5
salar <jafrisalar42@gmail.com>
2024-12-20 12:15:30 +0500

    config changes

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
-  base: "/react-pdf-highlighter/",
   build: {
     outDir: "dist",
   },
   plugins: [reactRefresh()],
   server: {
-    port: 3001,
+    port: 3002,
     host:'0.0.0.0'
   },
 });
